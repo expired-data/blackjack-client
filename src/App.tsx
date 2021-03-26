2fe668e1da43119ce79140520d13356ceef56757
A Ridley <expireddata@gmail.com>
2021-03-26 11:42:47 +0000

    initial client

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useClient } from "./client";
+import { CardDisplay } from "./components/CardDisplay";
+import { StatsPanel } from "./components/StatsPanel"; 
 
-function App() {
+const App = () => {
+  const [hand, client, stats] = useClient();
+  if (!hand || !client) {
+    return <>Loading...</>;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to Blackjack</h1>
+      <CardDisplay hand={hand} />
+      <div className="buttonContainer">
+        {hand.value <= 21 && <button onClick={() => client?.hit()}>Hit</button>}
+        <button className={hand.value > 21 ? "bust" : ""} onClick={() =>{ client.reset().then(() => client.stats()) }}>
+          {hand.value > 21 ? "Try again." : "Stick"}
+        </button>
+      </div>
+
+      <StatsPanel stats={stats} /> 
     </div>
   );
-}
+};
 
 export default App;
